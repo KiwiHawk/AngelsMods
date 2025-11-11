799abb42af48a651c91f66902d3e7d7d6796e0dc
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-11-11 23:23:49 +1300

    Flipped graphics (#1178)
    Updates graphics for induction furnace and casting machine - including flipped graphics.
    
    ---
    * feat: add vert/horiz pipe shadow patch util funcs
    
    * feat: add flippable induction furnace sprites
    
    * feat: add casting machine entity sprites w/flip
    
    * feat: add new icons
    
    * feat: updated metal materials, geometry fixes
    
    * feat: updated metal, geometry fixes, anim fixes
    
    ---------
    
    Co-authored-by: Kira <kirazy@live.com>

---
+++ angelssmeltinggraphics/graphics/entity/induction-furnace/induction-furnace-base-shadow-flipped.lua
@@ -0,0 +1,7 @@
+return
+{
+	width = 430,
+	height = 301,
+	shift = util.by_pixel_hr(54.0, 16.5),
+	line_length = 1,
+}
---
+++ angelssmeltinggraphics/graphics/entity/induction-furnace/induction-furnace-base-shadow.lua
@@ -0,0 +1,7 @@
+return
+{
+	width = 390,
+	height = 301,
+	shift = util.by_pixel_hr(29.0, 16.5),
+	line_length = 1,
+}
