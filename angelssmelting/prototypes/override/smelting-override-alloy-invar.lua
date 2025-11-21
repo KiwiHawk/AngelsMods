f668accb157c808fe28dc35154264cb911d0fdc0
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-11-21 19:14:11 +1300

    Move Bob's alloy prototype creation to data stage

--- angelssmelting/prototypes/override/smelting-override-alloy-invar.lua
+++ angelssmelting/prototypes/override/smelting-override-alloy-invar.lua
@@ -7,111 +7,18 @@ end
 -------------------------------------------------------------------------------
 -- PLATE ----------------------------------------------------------------------
 -------------------------------------------------------------------------------
-if angelsmods.trigger.smelting_products["invar"].plate then
-  if mods["bobplates"] then
+if mods["bobplates"] then
+  if angelsmods.trigger.smelting_products["invar"].plate then
     OV.hide_recipe("bob-invar-alloy")
-    data:extend({
-      --INVAR
-      --ITEMS
-      {
-        type = "fluid",
-        name = "angels-liquid-molten-invar",
-        icon = "__angelssmeltinggraphics__/graphics/icons/molten-invar.png",
-        icon_size = 64,
-        subgroup = "angels-alloys-casting",
-        order = "d[invar]-a[liquid-molten-invar]",
-        default_temperature = 0,
-        heat_capacity = "0kJ",
-        base_color = { r = 95 / 256, g = 125 / 256, b = 122 / 256 },
-        flow_color = { r = 95 / 256, g = 125 / 256, b = 122 / 256 },
-        max_temperature = 0,
-        auto_barrel = false,
-      },
-    })
-    data:extend({
-      --RECIPES
-      --SMELTING
-      {
-        type = "recipe",
-        name = "angels-liquid-molten-invar",
-        category = "angels-induction-smelting-2",
-        subgroup = "angels-alloys-casting",
-        energy_required = 4,
-        enabled = false,
-        ingredients = {
-          { type = "item", name = "angels-ingot-steel", amount = 24 },
-          { type = "item", name = "angels-ingot-nickel", amount = 12 },
-        },
-        results = {
-          { type = "fluid", name = "angels-liquid-molten-invar", amount = 360 },
-        },
-        order = "d[invar]-a[liquid-molten-invar]",
-        crafting_machine_tint = angelsmods.functions.get_fluid_recipe_tint("angels-liquid-molten-invar"),
-      },
-      --CASTING
-      {
-        type = "recipe",
-        name = "angels-plate-invar",
-        localised_name = { "item-name.bob-invar-alloy" },
-        category = "angels-casting-2",
-        subgroup = "angels-alloys-casting",
-        energy_required = 4,
-        enabled = false,
-        ingredients = {
-          { type = "fluid", name = "angels-liquid-molten-invar", amount = 40 },
-        },
-        results = {
-          { type = "item", name = "bob-invar-alloy", amount = 4 },
-        },
-        icons = angelsmods.functions.add_icon_layer(
-          {
-            {
-              icon = "__angelssmeltinggraphics__/graphics/icons/plate-invar.png",
-              icon_size = 32,
-            }
-          },
-          angelsmods.functions.get_object_icons("angels-liquid-molten-invar"),
-          { -10, -10 },
-          0.4375
-        ),
-        order = "d[invar]-b[invar-alloy]",
-      },
-      --TECH
-      {
-        type = "technology",
-        name = "angels-invar-smelting-1",
-        icon = "__angelssmeltinggraphics__/graphics/technology/casting-invar-tech.png",
-        icon_size = 256,
-        prerequisites = {
-          --"angels-metallurgy-2",
-          "angels-steel-smelting-1",
-          "angels-nickel-smelting-1",
-        },
-        effects = {
-          {
-            type = "unlock-recipe",
-            recipe = "angels-liquid-molten-invar",
-          },
-          {
-            type = "unlock-recipe",
-            recipe = "angels-plate-invar",
-          },
-        },
-        unit = {
-          count = 150,
-          ingredients = {
-            { "automation-science-pack", 1 },
-            { "logistic-science-pack", 1 },
-          },
-          time = 30,
-        },
-        order = "c-a",
-      },
-    })
     angelsmods.functions.move_item("bob-invar-alloy", "angels-alloys-casting", "d[invar]-b[invar-alloy]")
     data.raw["item"]["bob-invar-alloy"].icon = "__angelssmeltinggraphics__/graphics/icons/plate-invar.png"
     data.raw["item"]["bob-invar-alloy"].icon_size = 32
     OV.global_replace_technology("bob-invar-processing", "angels-invar-smelting-1")
     angelsmods.functions.allow_productivity("angels-plate-invar")
+  else
+    angelsmods.functions.hide("angels-liquid-molten-invar")
+    OV.disable_recipe({ "angels-liquid-molten-invar" })
+    OV.disable_recipe({ "angels-plate-invar" })
+    OV.disable_technology({ "angels-invar-smelting-1" })
   end
 end
