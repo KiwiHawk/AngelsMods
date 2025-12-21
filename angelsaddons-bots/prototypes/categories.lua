d55a2d6f3447e824e1329fa4d8d9beb1b84eaebe
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-12-21 17:57:08 +1300

    Don't add a separate item group in bots mod

--- angelsaddons-bots/prototypes/categories.lua
+++ angelsaddons-bots/prototypes/categories.lua
@@ -3,24 +3,6 @@
 ---------------------------------------------------------------------------
 data:extend({
   {
-    type = "item-group",
-    name = "angels-logistics",
-    order = "la[angels]-e[industries]-d",
-    icons = {
-      {
-        icon = "__angelsaddons-bots__/graphics/technology/cargo-roboport-tech.png",
-        icon_size = 128,
-        scale = 0.5,
-      },
-      {
-        icon = "__angelsaddons-bots__/graphics/icons/void.png",
-        icon_size = 32,
-        scale = 64 / 32 * 0.35,
-        shift = { 20, -20 },
-      },
-    },
-  },
-  {
     type = "item-subgroup",
     name = "angels-chests-big",
     group = "logistics",
@@ -29,19 +11,29 @@ data:extend({
   {
     type = "item-subgroup",
     name = "angels-cargo-ports",
-    group = "angels-logistics",
-    order = "bb[carbo-port]-a",
+    group = "logistics",
+    order = "f[carbo-port]-a",
   },
   {
     type = "item-subgroup",
     name = "angels-cargo-expander",
-    group = "angels-logistics",
-    order = "bb[carbo-port]-b",
+    group = "logistics",
+    order = "f[carbo-port]-b",
   },
   {
     type = "item-subgroup",
     name = "angels-cargo-bots",
-    group = "angels-logistics",
-    order = "cb[bots]",
+    group = "logistics",
+    order = "f[bots]",
   },
 })
+
+if mods["boblogistics"] then
+  for _, subgroup in pairs({
+    "angels-cargo-ports",
+    "angels-cargo-expander",
+    "angels-cargo-bots",
+  }) do
+    data.raw["item-subgroup"][subgroup].group = "bob-logistics"
+  end
+end
